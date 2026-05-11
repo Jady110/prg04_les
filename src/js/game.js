0edc0dc2c1cd438ac92fc691121b137dd59f8296
Jadybaby <1103637@hr.nl>
2026-05-11 13:48:35 +0200

    chuu's.

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,5 @@
 import '../css/style.css'
-import { Actor, Engine, Vector, DisplayMode } from "excalibur"
+import { Actor, Engine, Vector, DisplayMode, randomInRange } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
 
 export class Game extends Engine {
@@ -15,17 +15,34 @@ export class Game extends Engine {
     }
 
     startGame() {
+
         console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(500, 300)
-        fish.vel = new Vector(-10,0)
-        fish.events.on("exitviewport", (e) => this.fishLeft(e))
-        this.add(fish)
+
+        for (let i = 0; i < 100; i++) {
+            const fish = new Actor()
+            fish.graphics.use(Resources.Fish.toSprite())
+            fish.pos = new Vector(Math.random() * 1280, randomInRange(0, 720))
+            fish.vel = new Vector(randomInRange(-100, 100), randomInRange(-100, 100))
+            fish.events.on("exitviewport", (e) => this.fishLeft(e))
+            this.add(fish)
+        }
+
+        const shark = new Actor()
+        shark.graphics.use(Resources.Shark.toSprite())
+        shark.pos = new Vector(700, 100)
+        shark.vel = new Vector(-10, 0)
+        shark.events.on("exitviewport", (e) => this.sharkLeft(e))
+        shark.add(fish)
+
+        const water = new Actor()
+        water.graphics.use(Resources.Water.toSprite())
+        water.pos = new Vector(1280 / 2, 720 / 2)
+        water.z = -1;
+        this.add(water)
     }
 
     fishLeft(e) {
-        e.target.pos = new Vector(1350, 300)
+        e.target.pos = new Vector(Math.random() * 1280, randomInRange(0, 720))
     }
 }
 
